44a20b56a52038b572e4159337b1baddb1d25343
gabrielJGS <gabrielftjose@gmail.com>
2023-02-13 20:41:30 -0300

    upload first version

---
+++ src/components/card.tsx
@@ -0,0 +1,12 @@
+export default function card() {
+  return (
+    <div className="text-center shadow-lg p-10 rounded-xl my-10 bg-gray-100">
+      {/* <DiJsBadge width={100} height={100} className="text-[100px] mx-auto text-amber-400 rounded-xl" /> */}
+      <h3 className="text-lg font-medium pt-8 pb-2">Designs</h3>
+      <p className="py-2">Designs elegantes com html</p>
+      <h4 className="text-teal-600 py-4">Projetos relacionados</h4>
+      <p className="text-gray-800 py-1">lalalala</p>
+      <p className="text-gray-800 py-1">lelele</p>
+    </div>
+  );
+}
