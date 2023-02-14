155e8e2932b8a263c39e5a5bb0ad1c7051535ce1
gabrielJGS <gabrielftjose@gmail.com>
2023-02-14 11:36:10 -0300

    test akamai config

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,29 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   images: {
+    loader: "akamai",
+    path: isProd ? 'https://gabrieljs.online' : "/",
     // formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "gabrieljs.online",
-        port: "",
-        pathname: "/image/upload/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/image/upload/**",
-
-      }
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "gabrieljs.online",
+    //     port: "",
+    //     pathname: "/image/upload/**",
+    //   },
+    //   {
+    //     protocol: "http",
+    //     hostname: "localhost",
+    //     port: "3000",
+    //     pathname: "/image/upload/**",
+    //   }
+    // ],
   },
-  assetPrefix: "",
+  assetPrefix: isProd ? 'https://gabrieljs.online' : undefined,
 };
 
 export default nextConfig;
