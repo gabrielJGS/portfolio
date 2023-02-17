f9a63f032a331810039f0ae7c690a43a7eb5025b
gabrielJGS <gabrielftjose@gmail.com>
2023-02-17 20:19:51 -0300

    split project images into separate file

--- src/components/Projetos.tsx
+++ src/components/Projetos.tsx
@@ -12,61 +12,9 @@ import {
   SiReact,
   SiRealm,
 } from "react-icons/si";
+import { projetoRecordar, projetoEsclarecaImgs, projetoImgTranslator } from "../utils/projetosFotos";
 
 export const Projetos = () => {
-  const projetoEsclarecaImgs = [
-    {
-      original: "https://user-images.githubusercontent.com/82184751/132075958-d5299a6b-062a-47bc-9522-c9e321615418.png",
-    },
-    {
-      original: "https://user-images.githubusercontent.com/82184751/132076004-21bee89e-1314-407f-88be-a02d5d774eb4.png",
-    },
-    {
-      original: "https://user-images.githubusercontent.com/82184751/132075996-46fec8c0-dd23-4744-a6c9-7963dfa33d9e.png",
-    },
-    {
-      original: "https://user-images.githubusercontent.com/82184751/132076149-31aa6ac6-c1ff-4525-978b-195d4fdd5f9c.png",
-    },
-    {
-      original: "https://user-images.githubusercontent.com/82184751/132076163-fa385ec0-0da0-46ce-ae1f-a93b62ab4444.png",
-    },
-    {
-      original: "https://user-images.githubusercontent.com/82184751/132076228-9f66acb1-c59d-4150-a649-e03c4132c04f.png",
-    },
-  ];
-  const projetoImgTranslator = [
-    {
-      original: "https://user-images.githubusercontent.com/82184751/132076655-45e9d1cc-f8c0-4f5d-a19f-dca33f97fcff.png",
-    },
-    {
-      original: "https://user-images.githubusercontent.com/82184751/132076643-7ed2cc44-ebab-49c6-91e1-abefc52493cc.png",
-    },
-    {
-      original: "https://user-images.githubusercontent.com/82184751/132076666-8cb704d0-41b5-4089-9339-0088050494a3.png",
-    },
-  ];
-  const projetoRecordar = [
-    {
-      original:
-        "https://play-lh.googleusercontent.com/d0ELPSkrCFE4AjeN5NTip_Pl58TUIS8lLYfDjwaxBG43zKwQI8zF-yd-SAlKwhMHHkw=w2560-h1440-rw",
-    },
-    {
-      original:
-        "https://play-lh.googleusercontent.com/qtW8ZJ6Aem96hsrC_23bReDwvWsf87XkmkBhsMzySVHFMNasytoXSRJ1c9q4U5UC8zA=w2560-h1440-rw",
-    },
-    {
-      original:
-        "https://play-lh.googleusercontent.com/6VNNxmQ4iDUEDiXTIvWtnrMVjasUgW_zs9oRKpE2FfkmCk2tcf7H6QHA73lEWPzjx3Q=w2560-h1440-rw",
-    },
-    {
-      original:
-        "https://play-lh.googleusercontent.com/uGM8KSqT4gLiX-dlpsjpQFWSf35O6yuadxCvLERuq4iz__srFr1uXqZWopGgxa1rd0A=w2560-h1440-rw",
-    },
-    {
-      original:
-        "https://play-lh.googleusercontent.com/KX5fb7izegUos3lFszmNnqzthNBqjccjTm0Air0j9_KjcrGohxtI2OvI9Agl-funj7Us=w2560-h1440-rw",
-    },
-  ];
   return (
     <section id="projetos">
       <div>
