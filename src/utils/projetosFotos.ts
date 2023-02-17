5a634b807de503e39d3fa8d53c92ed1262edbb92
gabrielJGS <gabrielftjose@gmail.com>
2023-02-17 20:40:36 -0300

    downloaded photos into project

--- src/utils/projetosFotos.ts
+++ src/utils/projetosFotos.ts
@@ -1,55 +1,65 @@
+import esclareca1 from "../../public/imgs/esclareca/1.png";
+import esclareca2 from "../../public/imgs/esclareca/2.png";
+import esclareca3 from "../../public/imgs/esclareca/3.png";
+import esclareca4 from "../../public/imgs/esclareca/4.png";
+import esclareca5 from "../../public/imgs/esclareca/5.png";
+import esclareca6 from "../../public/imgs/esclareca/6.png";
+
+import translator1 from "../../public/imgs/translator/1.png";
+import translator2 from "../../public/imgs/translator/2.png";
+import translator3 from "../../public/imgs/translator/3.png";
+
+import recordar1 from "../../public/imgs/recordar/1.webp";
+import recordar2 from "../../public/imgs/recordar/2.webp";
+import recordar3 from "../../public/imgs/recordar/3.webp";
+import recordar4 from "../../public/imgs/recordar/4.webp";
+import recordar5 from "../../public/imgs/recordar/5.webp";
+
 export const projetoEsclarecaImgs = [
   {
-    original: "https://user-images.githubusercontent.com/82184751/132075958-d5299a6b-062a-47bc-9522-c9e321615418.png",
+    original: esclareca1.src,
   },
   {
-    original: "https://user-images.githubusercontent.com/82184751/132076004-21bee89e-1314-407f-88be-a02d5d774eb4.png",
+    original: esclareca2.src,
   },
   {
-    original: "https://user-images.githubusercontent.com/82184751/132075996-46fec8c0-dd23-4744-a6c9-7963dfa33d9e.png",
+    original: esclareca3.src,
   },
   {
-    original: "https://user-images.githubusercontent.com/82184751/132076149-31aa6ac6-c1ff-4525-978b-195d4fdd5f9c.png",
+    original: esclareca4.src,
   },
   {
-    original: "https://user-images.githubusercontent.com/82184751/132076163-fa385ec0-0da0-46ce-ae1f-a93b62ab4444.png",
+    original: esclareca5.src,
   },
   {
-    original: "https://user-images.githubusercontent.com/82184751/132076228-9f66acb1-c59d-4150-a649-e03c4132c04f.png",
+    original: esclareca6.src,
   },
 ];
 export const projetoImgTranslator = [
   {
-    original: "https://user-images.githubusercontent.com/82184751/132076655-45e9d1cc-f8c0-4f5d-a19f-dca33f97fcff.png",
+    original: translator1.src,
   },
   {
-    original: "https://user-images.githubusercontent.com/82184751/132076643-7ed2cc44-ebab-49c6-91e1-abefc52493cc.png",
+    original: translator2.src,
   },
   {
-    original: "https://user-images.githubusercontent.com/82184751/132076666-8cb704d0-41b5-4089-9339-0088050494a3.png",
+    original: translator3.src,
   },
 ];
 export const projetoRecordar = [
   {
-    original:
-      "https://play-lh.googleusercontent.com/d0ELPSkrCFE4AjeN5NTip_Pl58TUIS8lLYfDjwaxBG43zKwQI8zF-yd-SAlKwhMHHkw=w2560-h1440-rw",
+    original: recordar1.src,
   },
   {
-    original:
-      "https://play-lh.googleusercontent.com/qtW8ZJ6Aem96hsrC_23bReDwvWsf87XkmkBhsMzySVHFMNasytoXSRJ1c9q4U5UC8zA=w2560-h1440-rw",
+    original: recordar2.src,
   },
   {
-    original:
-      "https://play-lh.googleusercontent.com/6VNNxmQ4iDUEDiXTIvWtnrMVjasUgW_zs9oRKpE2FfkmCk2tcf7H6QHA73lEWPzjx3Q=w2560-h1440-rw",
+    original: recordar3.src,
   },
   {
-    original:
-      "https://play-lh.googleusercontent.com/uGM8KSqT4gLiX-dlpsjpQFWSf35O6yuadxCvLERuq4iz__srFr1uXqZWopGgxa1rd0A=w2560-h1440-rw",
+    original: recordar4.src,
   },
   {
-    original:
-      "https://play-lh.googleusercontent.com/KX5fb7izegUos3lFszmNnqzthNBqjccjTm0Air0j9_KjcrGohxtI2OvI9Agl-funj7Us=w2560-h1440-rw",
+    original: recordar5.src,
   },
 ];
-
-
